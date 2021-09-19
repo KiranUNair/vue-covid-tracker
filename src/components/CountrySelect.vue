c7b2751d7b48d6f310c04d2542f97c930e0b094a
KiranUNair <kirannair.12689@gmail.com>
2021-09-19 15:59:46 +0530

    vue covid tracker
    vue covid tracker front end application developed using Vue and Tailwind

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,24 @@
+<template>
+    <select v-model="selected" class="form-select mt-10 block w-full rounded border p-3" @change="onChange">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :key="country.Country" :value="country.ID">{{country.Country}}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data(){
+        return {
+            selected: 0
+        }    
+    },
+    methods: {
+        onChange(){
+            const selectedCountry = this.countries.find((item) => item.ID === this.selected);
+            this.$emit('get-country', selectedCountry);
+        }
+    }
+}
+</script>
